a0b1e24c171d26470f4d22d88db96ed033195888
Frank <frankmuema42@gmail.com>
2026-03-31 22:58:56 +0300

    Implementing Logging and Middleware

--- src/app.js
+++ src/app.js
@@ -1,10 +1,34 @@
 import express from 'express';
-
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+    
 // Initialize app
 const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+
+app.use(morgan('combined', {
+    stream: {
+        write: (message) => logger.info(message.trim()),
+    },
+}));
 
 app.get('/', (req, res) => {
+    logger.info('Hello from the Events API!');
     res.send('Welcome to the Events API');
 })
+
+// Health check
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'OK', uptime: process.uptime(), timestamp: new Date().toISOString() });
+});
+
+app.get('/api', (req, res) => {
+    res.status(200).json({ message: 'API is working' });
+});
 
 export default app;
